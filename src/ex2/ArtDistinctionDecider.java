dbc8a2db7c216a5df99c5d5f89eda4084887c095
TheoBikoko <25634122r@iespoblenou.org>
2025-03-27 22:44:34 +0100

    Work in progress

--- src/ex2/ArtDistinctionDecider.java
+++ src/ex2/ArtDistinctionDecider.java
@@ -4,7 +4,7 @@ public class ArtDistinctionDecider implements DistinctionDecider{
     @Override
     public void evaluateDistinction(Student student) {
         if (student.score > 70) {
-            System.out.println(student.regNumber + " has received a distinction in science.");
+            System.out.println(student.regNumber + " has received a distinction in arts.");
         }
     }
 }
--- src/ex2/Client.java
+++ src/ex2/Client.java
@@ -14,24 +14,38 @@ class Client {
         }
 
         System.out.println("===Distinctions:===");
-//        DistinctionDecider distinctionDecider = new DistinctionDecider();
-        // Evaluate distinctions.          
+        DistinctionDecider artDistinctionDecider = new ArtDistinctionDecider();
+        DistinctionDecider scienceDistinctionDecider = new ScienceDistinctionDecider();
+
         for (Student student : enrolledStudents) {
-//            distinctionDecider.evaluateDistinction(student);
+            if (student instanceof ArtStudent){
+                artDistinctionDecider.evaluateDistinction(student);
+            } else if (student instanceof ScienceStudent) {
+                scienceDistinctionDecider.evaluateDistinction(student);
+            }
         }
     }
 
     private static List<Student> enrollStudents() {
-//        Student sam = new Student("Sam", "R1", 81.5, "Comp..");
-        Student bob = new ScienceStudent("Bob", "R2", 72, "Physics");
-        Student john = new ArtStudent("John", "R3", 71, "History");
-//        Student kate = new Student("Kate", "R4", 66.5, "English");
+        Student sam = new ScienceStudent("Sam", "R1", 81.5, "Chemistry");
+        Student bob = new ScienceStudent("Bob", "R2", 82, "Physics");
+        Student john = new ArtStudent("John", "R3", 90, "History");
+        Student kate = new ArtStudent("Kate", "R4", 86.5, "Poetry");
+        Student jason = new ScienceStudent("Jason", "R5", 31.5, "Chemistry");
+        Student khabib = new ScienceStudent("Khabib", "R6", 62, "Physics");
+        Student sogyo = new ArtStudent("Sogyo", "R7", 80, "History");
+        Student zhang = new ArtStudent("Zhang", "R8", 41.5, "Poetry");
 
-        List<Student> students = new ArrayList<Student>();
-//        students.add(sam);
+        List<Student> students = new ArrayList<>();
+        students.add(sam);
         students.add(bob);
         students.add(john);
-//        students.add(kate);
+        students.add(kate);
+        students.add(jason);
+        students.add(khabib);
+        students.add(sogyo);
+        students.add(zhang);
+
         return students;
     }
 }
